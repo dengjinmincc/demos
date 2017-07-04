e67a10b5478bbf28ac491f0b71a8442c7ee5b714
bcqz <1436563611@qq.com>
2017-07-04 16:50:04 +0800

    本地天气预报

--- localWeather/js/index.js
+++ localWeather/js/index.js
@@ -1,5 +1,5 @@
 //appkey:ff8591e26fee2f9902562d1332f683f2
-$(document).ready(function(){
+/*$(document).ready(function(){
 	var temp = $("#temp"), direction = $("#direction"), strength = $("#strength"), 
 		humidity = $("#humidity"), time = $("#time"), update = $("#update");
 		
@@ -21,4 +21,30 @@ $(document).ready(function(){
 			}
 		});
 	})
-})
+})*/
+var index = document.getElementById("index"), 
+	future = document.getElementById("future"), 
+	nav = document.getElementsByTagName("a");
+	
+nav[0].onclick = function(e){
+	e.preventDefault();
+	index.style.display = "block";
+	future.style.display = "none";
+	nav[0].style.backgroundColor = "#fff";
+	nav[0].style.color = "#4990E2";
+	nav[1].style.backgroundColor = "transparent";
+	nav[1].style.color = "#fff";
+}
+nav[1].onclick = function(e){
+	e.preventDefault();
+	index.style.display = "none";
+	future.style.display = "block";
+	nav[1].style.backgroundColor = "#fff";
+	nav[1].style.color = "#4990E2";
+	nav[0].style.backgroundColor = "transparent";
+	nav[0].style.color = "#fff";
+}
+
+function showWeather(response){
+	
+}
