750c4beebd3e85c998da63984fab63f23ede225d
bcqz <1436563611@qq.com>
2017-07-14 09:23:04 +0800

    twitchTV

--- twitchTV/js/index.js
+++ twitchTV/js/index.js
@@ -1,25 +1,8 @@
 $(document).ready(function(){
-	var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb", "noobs2ninjas","beohoff"],  
-		main = $(".main"); 
-		
+	var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb", "noobs2ninjas","beohoff"];   
 		
 	//根据channel查找其头像等,显示在列表中	
 	channels.forEach(function(channel){
-		var status;
-		
-		/*$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channel + '?callback=?', function(data){
-			//console.log(data);
-			if(data.stream == null){
-				game = "offline";
-				status = "offline";
-			}else if(data.stream == undefined){
-				game = "Account Closed";
-				status = "offline";
-			}else{
-				game = data.stream.game;
-				status = "online";
-			}
-		});*/
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + channel + '?callback=?', function(data){
 			console.log(data);
 			var logo = data.logo != null? data.logo : "http://www.fashatude.com/static/fashatude/img/user_icon.png",
@@ -37,14 +20,12 @@ $(document).ready(function(){
 			item.appendChild(pic);
 			item.appendChild(links);
 			
-			//temp += "<li><img src = " + logo + ">" + "<a href = " + data._links.self + "> "+ name + "</a>" + "</li>";
-			//console.log(temp[0]);
-			/*if(status != null){
+			if(status != null){
 				i.className = "online";
 			}else{
 				i.className = "offline";
 			}
-			item.appendChild(i);*/
+			item.appendChild(i);
 			main.appendChild(item);
 		})
 	})
@@ -52,5 +33,43 @@ $(document).ready(function(){
 	//点击不同状态时,根据其status进行切换
 	//点击all 全部展示
 	//点击online status为null的将其display设置为none
+	
+	var status1 = document.getElementById("status"),
+		main = document.getElementById("main"), 
+		all = document.getElementById("all"), 
+		on = document.getElementById("on"),
+		off = document.getElementById("off"), 
+		itemss = status1.getElementsByTagName("li"),
+		user = main.getElementsByTagName("li"),
+		i1 = document.getElementsByTagName("i"), 
+		len1 = itemss.length;
+	
+	for(let i = 0; i < len1; i++){
+		itemss[i].onclick = function(e){
+			if(e.target.className == "initial"){
+				for(let j = 0; j < len1; j++){
+					itemss[j].className = "initial";
+				}
+				itemss[i].className = "convert";
+				if(itemss[i] == on){
+					for(let k = 0; k < user.length; k++){
+						if(i1[k].className == "offline"){
+							user[k].style.display = "none";
+						}
+					}
+				}else if(itemss[i] == off){
+					for(let k = 0; k < user.length; k++){
+						if(i1[k].className == "online"){
+							user[k].style.display = "none";
+						}
+					}
+				}else{
+					for(let k = 0; k < user.length; k++){	
+						user[k].style.display = "block";
+					}
+				}
+				}
+			}
+		}
 })
 
